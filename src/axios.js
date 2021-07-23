5c03344ed5cbce57511a025b4a389e36adbaca1a
Adarsh Kunwar <adarsh.infinite@gmail.com>
2021-07-24 02:52:02 +0530

    deployment done

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
   // THE API (cloud function) URL
-    baseURL: 'http://localhost:5001/challenge-603b1/us-central1/api' // api(cloud function) url
+  baseURL: "https://us-central1-challenge-603b1.cloudfunctions.net/api"  
+  //baseURL: 'http://localhost:5001/challenge-603b1/us-central1/api' // api(cloud function) url
 });
 
 export default instance;
